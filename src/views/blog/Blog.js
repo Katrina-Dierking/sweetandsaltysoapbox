295129fca1c87d4942118f3ccfa65dd11c310334
Katrina <kalynndier@gmail.com>
2023-07-10 12:50:26 -0400

    blog side bar

--- src/views/blog/Blog.js
+++ src/views/blog/Blog.js
@@ -6,78 +6,85 @@ const Blog = () => {
   return (
     <Layout>
       <section className="blogBody">
+      <section className="blogContainer">
+      
         <section className="blog">
-          <h2>Blog One</h2>
-          <p>
+            <h2>Blog One</h2>
+            <p>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint ea,
             esse, aut maxime eaque officiis repellendus reprehenderit ut
             possimus magni quam? Modi earum aperiam libero sapiente est aut
             doloremque porro.
-          </p>
+            </p>
         </section>
         <section className="blog">
-          <h2>Blog One</h2>
-          <p>
+            <h2>Blog One</h2>
+            <p>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint ea,
             esse, aut maxime eaque officiis repellendus reprehenderit ut
             possimus magni quam? Modi earum aperiam libero sapiente est aut
             doloremque porro.
-          </p>
+            </p>
         </section>
         <section className="blog">
-          <h2>Blog One</h2>
-          <p>
+            <h2>Blog One</h2>
+            <p>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint ea,
             esse, aut maxime eaque officiis repellendus reprehenderit ut
             possimus magni quam? Modi earum aperiam libero sapiente est aut
             doloremque porro.
-          </p>
+            </p>
         </section>
         <section className="blog">
-          <h2>Blog One</h2>
-          <p>
+            <h2>Blog One</h2>
+            <p>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint ea,
             esse, aut maxime eaque officiis repellendus reprehenderit ut
             possimus magni quam? Modi earum aperiam libero sapiente est aut
             doloremque porro.
-          </p>
+            </p>
         </section>
         <section className="blog">
-          <h2>Blog One</h2>
-          <p>
+            <h2>Blog One</h2>
+            <p>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint ea,
             esse, aut maxime eaque officiis repellendus reprehenderit ut
             possimus magni quam? Modi earum aperiam libero sapiente est aut
             doloremque porro.
-          </p>
+            </p>
         </section>
         <section className="blog">
-          <h2>Blog One</h2>
-          <p>
+            <h2>Blog One</h2>
+            <p>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint ea,
             esse, aut maxime eaque officiis repellendus reprehenderit ut
             possimus magni quam? Modi earum aperiam libero sapiente est aut
             doloremque porro.
-          </p>
+            </p>
         </section>
         <section className="blog">
-          <h2>Blog One</h2>
-          <p>
+            <h2>Blog One</h2>
+            <p>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint ea,
             esse, aut maxime eaque officiis repellendus reprehenderit ut
             possimus magni quam? Modi earum aperiam libero sapiente est aut
             doloremque porro.
-          </p>
+            </p>
         </section>
         <section className="blog">
-          <h2>Blog One</h2>
-          <p>
+            <h2>Blog One</h2>
+            <p>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint ea,
             esse, aut maxime eaque officiis repellendus reprehenderit ut
             possimus magni quam? Modi earum aperiam libero sapiente est aut
             doloremque porro.
-          </p>
+            </p>
         </section>
+
+        </section>
+        <aside>
+            <h3>Previous posts:</h3>
+        </aside>
       </section>
     </Layout>
   );
