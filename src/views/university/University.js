e05d520939b6b39d5a1b690e6893d509e711bdc9
Katrina <kalynndier@gmail.com>
2023-07-11 12:44:43 -0400

    university

--- src/views/university/University.js
+++ src/views/university/University.js
@@ -1,10 +1,29 @@
 import React from 'react'
 import Layout from '../../components/layout/Layout'
+import {ssu} from '../../assets/images/index'
+import '../../assets/styles/university.scss'
+
 
 const University = () => {
   return (
     <Layout title="University">
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim eius omnis obcaecati praesentium quis veniam sed nam! Expedita suscipit, dolores molestiae illo reprehenderit ullam, laborum nemo deserunt autem, aliquam aperiam.</p>
+    <main>
+    <section className='uniHeader'>
+      <img src={ssu} alt="sweet and salty university"></img>
+        <h2>Sweet & Salty Soapbox University</h2>
+
+
+        <section className='inputSec'>
+        
+          <input type="text" placeholder='Email'></input>
+          <input type="password" placeholder='Password'></input>
+          <button type="submit">Login</button>
+        </section>
+
+
+    
+    </section>
+    </main>
     </Layout>
   )
 }
