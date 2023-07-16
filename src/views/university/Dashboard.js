db6b96d839ab4f0290c20507204b43ea98f904e4
Katrina <kalynndier@gmail.com>
2023-07-15 22:06:20 -0400

    dashboard

--- src/views/university/Dashboard.js
+++ src/views/university/Dashboard.js
@@ -1,9 +1,48 @@
 import React from 'react'
+import { qbu } from "../../assets/images/index";
+import "../../assets/styles/university.scss";
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
-  )
+    <section className="dashboardContainer">
+      <section className="uniInfo">
+        <form>
+          <div>
+            <label for="courses">Paid Courses: </label>
+            <select name="courses" id="courses">
+              <option value="soap-making">Soap Making for Beginners</option>
+              <option value="soap-making-business">
+                Building a Soap Making Business
+              </option>
+              <option value="online-business">
+                Starting an Online Business
+              </option>
+              <option value="build-website">
+                Building Your Own Website From Scratch
+              </option>
+            </select>
+          </div>
+
+          <div>
+            <label for="campus-options">Campus Options:</label>
+            <select name="campus" id="campus">
+              <option value="self-pace">Self-Paced</option>
+              <option value="live-group">Live Group</option>
+            </select>
+          </div>
+        </form>
+      </section>
+
+      <section className="uniHeader">
+        <img src={qbu} alt="queen bean design university"></img>
+        <h2>Queen Bean Design University</h2>
+      </section>
+
+      <section className="freeCourseSection">
+        <h3>Enjoy the free courses and resources below:</h3>
+      </section>
+    </section>
+  );
 }
 
 export default Dashboard
--- src/views/university/University.js
+++ src/views/university/University.js
@@ -15,35 +15,7 @@ const University = () => {
         <img src={qbu} alt="queen bean design university"></img>
         <h2>Queen Bean Design University</h2>
       </section>
-      <section className="uniInfo">
-        <form>
-          <div>
-            <label for="courses">Courses: </label>
-            <select name="courses" id="courses">
-              <option value="soap-making">Soap Making for Beginners</option>
-              <option value="soap-making-business">
-                Building a Soap Making Business
-              </option>
-              <option value="online-business">
-                Starting an Online Business
-              </option>
-              <option value="build-website">
-                Building Your Own Website From Scratch
-              </option>
-            </select>
-          </div>
 
-          <div>
-            <label for="campus-options">Campus Options:</label>
-            <select name="campus" id="campus">
-              <option value="self-pace">Self-Paced</option>
-              <option value="live-group">Live Group</option>
-            </select>
-          </div>
-
-          <button>Login</button>
-        </form>
-      </section>
       <Reg />
       <hr></hr>
 
